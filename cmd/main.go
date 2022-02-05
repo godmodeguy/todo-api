1b77f37ccd5c617fd2d3785bc2a5017338e57192
wintermute <godmodguy@gmail.com>
2022-02-06 00:12:07 +0300

    change .env to yaml

--- cmd/main.go
+++ cmd/main.go
@@ -1,31 +1,19 @@
 package main
 
 import (
-	"github.com/joho/godotenv"
 	todo "learn/todoapi/pkg"
 	"learn/todoapi/pkg/handler"
+	"learn/todoapi/pkg/models"
+	"learn/todoapi/pkg/service"
 	"log"
-	"strconv"
 )
 
 func main() {
-	var port int64
-	var envs map[string]string
-	var err error
 
-	if envs, err = godotenv.Read(".env"); err != nil {
-		log.Fatal("error loading .env file")
-	}
-	log.Println(".env file loaded")
-
-	if port, err = strconv.ParseInt(envs["PORT"], 10, 32); err != nil {
-		log.Fatal("error loading port from .env")
-	}
-	log.Println("port set to ", port)
-
-	handler := handler.NewHandler()
-
-	server := todo.NewServer(int(port), handler.InitRoutes())
+	repo := models.NewRepository()
+	service := service.NewService(repo)
+	handler := handler.NewHandler(service)
+	server := todo.NewServer("8090", handler.InitRoutes())
 
 	if err := server.Run(); err != nil {
 		log.Fatal("error ocured while running server: ", err.Error())
