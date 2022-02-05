8b11fdd83e02163b178fab0f8b3974c6d8043945
wintermute <godmodguy@gmail.com>
2022-02-06 00:37:59 +0300

    tidy

--- cmd/main.go
+++ cmd/main.go
@@ -6,14 +6,21 @@ import (
 	"learn/todoapi/pkg/models"
 	"learn/todoapi/pkg/service"
 	"log"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatal("error loading config")
+	}
 
 	repo := models.NewRepository()
 	service := service.NewService(repo)
 	handler := handler.NewHandler(service)
-	server := todo.NewServer("8090", handler.InitRoutes())
+	server := todo.NewServer(viper.GetString("port"), handler.InitRoutes())
 
 	if err := server.Run(); err != nil {
 		log.Fatal("error ocured while running server: ", err.Error())
