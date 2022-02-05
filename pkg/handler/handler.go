7f2791a388e0af82e8c465cbc36f40fbefb670e5
wintermute <godmodguy@gmail.com>
2022-02-05 22:57:19 +0300

    setup auth and api routes

---
+++ pkg/handler/handler.go
@@ -0,0 +1,45 @@
+package handler
+
+import (
+	"container/list"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	r := gin.Default()
+
+	auth := r.Group("/auth")
+	{
+		auth.POST("/sign-in")
+		auth.POST("/sign-up")
+	}
+
+
+	api := r.Group("/api")
+	{
+		lists := api.Group("/lists")
+		{
+			lists.POST("/")
+			lists.GET("/")
+			lists.GET("/:id")
+			lists.PUT("/:id")
+			lists.DELETE("/:id")
+
+			tasks := lists.Group("/")
+			{
+				tasks.POST("/")
+				tasks.GET("/")
+				tasks.GET("/:task_id")
+				tasks.PUT("/:task_id")
+				tasks.DELETE("/:task_id")
+			}
+		}
+	}
+
+	return r
+}
